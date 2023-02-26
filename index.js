6d8c6b9f58e0b7103f51fec8d976e8fa8b4b81c5
Sofia Dzyuba <Sofia.Dzyuba@tptlive.ee>
2023-02-26 19:07:54 +0200

    3

--- index.js
+++ index.js
@@ -4,8 +4,8 @@ import boxen from 'boxen';
 console.log(chalk.blue.bgGreen('Hello chalk'));
 
 console.log(boxen(chalk.black.bgGreen('sofia'),{
-    padding: 1,
-    margin: 3,
+    padding: 16,
+    margin: 16,
     borderStyle: 'arrow',
     title: 'Need',
     titleAlignment: 'left'
@@ -13,16 +13,39 @@ console.log(boxen(chalk.black.bgGreen('sofia'),{
 
 console.log(boxen(chalk.black.bgRedBright('Dzyuba'),{
     padding: 6,
-    margin: 1,
-    borderStyle: 'classic',
-    title: 'On',
+    margin: 21,
+    borderStyle: 'bold',
+    title: 'on',
     titleAlignment: 'bottom'
 }));
 
 console.log(boxen(chalk.black.bgYellow.underline('MM-21'),{
     padding: 3,
-    margin: 1,
+    margin: 2,
     borderStyle: 'double',
     title: 'Boxid',
     titleAlignment: 'center'
+}));
+
+console.log(boxen(chalk.black.bgBlack('abstraktsioon'),{
+    padding: 19,
+    margin: 1,
+    borderStyle: 'singleDouble',
+    title: 'mis',
+    titleAlignment: 'center'
+}));
+
+console.log(boxen(chalk.black.bgBlue('567'),{
+    padding: 5,
+    margin: 6,
+    borderStyle: 'classic',
+    title: 'on',
+    titleAlignment: 'top'
+}));
+console.log(boxen(chalk.black.bgWhite('Frank Sinatra'),{
+    padding: 9,
+    margin: 3,
+    borderStyle: 'round',
+    title: 'ilusad',
+    titleAlignment: 'bottom'
 }));
